1ab46bf1c71e17dc99724ba6cef2fb009f53b4bb
ppzeng <1269010686@qq.com>
2017-07-21 13:22:32 +0800

    grocery

---
+++ src/main/java/com/virtual/grocery/web/controller/OrderListController.java
@@ -0,0 +1,61 @@
+/*
+ * =============================================================================
+ * 
+ *   Copyright (c) 2011-2016, The THYMELEAF team (http://www.thymeleaf.org)
+ * 
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * 
+ * =============================================================================
+ */
+package com.virtual.grocery.web.controller;
+
+import com.virtual.grocery.business.entities.Order;
+import com.virtual.grocery.business.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.thymeleaf.ITemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+@Controller
+public class OrderListController implements IGTVGController {
+
+    
+    public OrderListController() {
+        super();
+    }
+    @Autowired
+    private OrderService orderService;
+    @RequestMapping("/order/list")
+    public String process(
+            final HttpServletRequest request, final HttpServletResponse response,
+            Model model, HttpSession session)
+            throws Exception {
+
+        final List<Order> allOrders = orderService.findAll();
+        
+
+        model.addAttribute("orders", allOrders);
+        
+         return  "order/list";
+        
+    }
+
+}
